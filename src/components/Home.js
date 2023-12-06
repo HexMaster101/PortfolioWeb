c6274c8bc26c8495acc4a562f1dca28f66a2c3eb
HexMaster101 <luckygupta9219@gmail.com>
2023-12-06 17:02:04 +0530

    added animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
 	return (
 		<>
 			<section id="home-background">
 				<div className="container home-background">
-					<p className=" home-text">Home/{">"}</p>
-					<div className="intro">
+					<motion.p
+						variants={{
+							hidden: { opacity: 0, y: -25 },
+							visible: { opacity: 1, y: 0 },
+						}}
+						initial="hidden"
+						animate="visible"
+						transition={{ delay: 0.25 }}
+						className=" home-text"
+					>
+						Home/{">"}
+					</motion.p>
+					<motion.div
+						variants={{
+							hidden: { opacity: 0, x: 100 },
+							visible: { opacity: 1, x: 0 },
+						}}
+						initial="hidden"
+						animate="visible"
+						transition={{ delay: 0.65 }}
+						className="intro"
+					>
 						<h1 className="intro-text">
 							Hii, My Name Is <span className="my-name">Tanishk Gupta</span>
 						</h1>
@@ -15,7 +36,7 @@ export default function Home() {
 							webpages/web-apps.
 						</h1>
 						<p className="intro-para">Let me show You...</p>
-					</div>
+					</motion.div>
 				</div>
 			</section>
 		</>
